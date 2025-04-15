b2a31d26bd290ef1f6fc7f395aa9055b955ea859
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-04-15 20:52:23 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,41 +1,15 @@
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "project_name" {
-  description = "Name prefix for resources"
-  type        = string
-  default     = "my-app"
-}
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_cidr" {}
 variable "azs" {
-  description = "Availability Zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  type = list(string)
 }
-
 variable "public_subnets" {
-  description = "Public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type = list(string)
 }
-
 variable "private_subnets" {
-  description = "Private subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  type = list(string)
+}
+variable "aws_region" {
+  default = "us-east-1"
 }
