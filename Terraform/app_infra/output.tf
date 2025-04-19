631deeebde8ebeb1ef53b61e5b2215b062733f65
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-04-19 11:49:48 +0530

    Update output.tf

--- Terraform/app_infra/output.tf
+++ Terraform/app_infra/output.tf
@@ -1,39 +1,23 @@
-# outputs.tf
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value = aws_vpc.main.id
 }
 
 output "public_subnets" {
-  description = "List of public subnet IDs"
-  value       = module.vpc.public_subnets
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnets" {
-  description = "List of private subnet IDs"
-  value       = module.vpc.private_subnets
+  value = aws_subnet.private[*].id
 }
 
 output "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.main.name
 }
 
 output "ecr_repo_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.app.repository_url
+  value = aws_ecr_repository.app.repository_url
 }
 
 output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.app_bucket.bucket
+  value = aws_s3_bucket.app_bucket.bucket
 }
-
-output "ecs_task_execution_role_arn" {
-  description = "ARN of the ECS task execution role"
-  value       = aws_iam_role.ecs_task_execution.arn
-}
-
-output "ecs_service_name" {
-  description = "Name of the ECS service"
-  value
