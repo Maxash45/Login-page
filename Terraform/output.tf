f27a5074cb0a4d061ba9d6c2d6d495ad36d8b4f6
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-04-19 11:46:33 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,23 +1,24 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.app_infra.vpc_id  # Changed from module.vpc to module.app_infra
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  value = module.app_infra.public_subnets
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = module.app_infra.private_subnets
 }
 
+# Only include these if they're defined in your app_infra module
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
+  value = module.app_infra.ecs_cluster_name
 }
 
 output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
+  value = module.app_infra.ecr_repo_url
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.app_bucket.bucket
+  value = module.app_infra.s3_bucket_name
 }
