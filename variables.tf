bd61925a326ebd348a65237211eb00f43491dcaa
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-04-19 10:47:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_cidr" {}
+variable "azs" {
+  type = list(string)
+}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+variable "aws_region" {
+  default = "us-east-1"
+}
