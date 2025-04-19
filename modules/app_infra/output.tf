79d2d4d3ac7958b0486def5c15bf3798be7e5e62
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-04-19 11:08:23 +0530

    Update output.tf

--- modules/app_infra/output.tf
+++ modules/app_infra/output.tf
@@ -2,14 +2,22 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
+output "public_subnets" {
+  value = module.vpc.public_subnets
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.app_bucket.bucket
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
 }
 
 output "ecr_repo_url" {
   value = aws_ecr_repository.app.repository_url
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app_bucket.bucket
 }
